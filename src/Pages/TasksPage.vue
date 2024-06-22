3f5bb532b2884bbbe2e053644e75610e50a5f566
jonathanaudu007 <jaudu2@gmail.com>
2024-06-22 23:58:38 +0100

    added new componet to insert data

--- src/Pages/TasksPage.vue
+++ src/Pages/TasksPage.vue
@@ -1,69 +1,92 @@
 <template>
-    <main>
-        <div class="task-input">
-            <input type="text" placeholder="+ Add new task. Press enter to save." id="new-task" />
-        </div>
-        <div class="task-container">
-            <h2>My Tasks</h2>
-            <ul class="task-list">
-                <!-- Uncompleted tasks -->
-                <li v-for="task in uncompletedTasks" :key="task.id" :class="{ complete: task.is_completed }">
-                    <div>
-                        <input type="checkbox" :id="'task-' + task.id" :checked="task.is_completed" />
-                        <label :for="'task-' + task.id">{{ task.name }}</label>
-                    </div>
-                    <span class="task-icons">
-                        <i class="fas fa-edit"></i>
-                        <i class="fas fa-trash-alt"></i>
-                    </span>
-                </li>
-            </ul>
+  <main>
+    <NewTask @added="handleAddedTask" />
 
-            <div id="comButton" v-show="showToggleCompleteBtn">
-                <button @click="showCompletedTask = !showCompletedTask">
-                    <span v-if="!showCompletedTask">Show Completed Tasks</span>
-                    <span v-else>Hide Completed Tasks</span>
-                </button>
-                <hr />
-            </div>
+    <div class="task-container">
+      <h2>My Tasks</h2>
+      <ul class="task-list">
+        <!-- Uncompleted tasks -->
+        <li
+          v-for="task in uncompletedTasks"
+          :key="task.id"
+          :class="{ complete: task.is_completed }"
+        >
+          <div>
+            <input
+              type="checkbox"
+              :id="'task-' + task.id"
+              :checked="task.is_completed"
+            />
+            <label :for="'task-' + task.id">{{ task.name }}</label>
+          </div>
+          <span class="task-icons">
+            <i class="fas fa-edit"></i>
+            <i class="fas fa-trash-alt"></i>
+          </span>
+        </li>
+      </ul>
 
-            <ul class="task-list" v-if="showCompletedTask">
-                <!-- Completed tasks -->
-                <li v-for="task in completedTasks" :key="task.id" :class="{ complete: task.is_completed }">
-                    <div>
-                        <input type="checkbox" :id="'task-' + task.id" :checked="task.is_completed" />
-                        <label :for="'task-' + task.id">{{ task.name }}</label>
-                    </div>
-                    <span class="task-icons">
-                        <i class="fas fa-edit"></i>
-                        <i class="fas fa-trash-alt"></i>
-                    </span>
-                </li>
-            </ul>
-        </div>
-    </main>
+      <div id="comButton" v-show="showToggleCompleteBtn">
+        <button @click="showCompletedTask = !showCompletedTask">
+          <span v-if="!showCompletedTask">Show Completed Tasks</span>
+          <span v-else>Hide Completed Tasks</span>
+        </button>
+        <hr />
+      </div>
+
+      <ul class="task-list" v-if="showCompletedTask">
+        <!-- Completed tasks -->
+        <li
+          v-for="task in completedTasks"
+          :key="task.id"
+          :class="{ complete: task.is_completed }"
+        >
+          <div>
+            <input
+              type="checkbox"
+              :id="'task-' + task.id"
+              :checked="task.is_completed"
+            />
+            <label :for="'task-' + task.id">{{ task.name }}</label>
+          </div>
+          <span class="task-icons">
+            <i class="fas fa-edit"></i>
+            <i class="fas fa-trash-alt"></i>
+          </span>
+        </li>
+      </ul>
+    </div>
+  </main>
 </template>
-<script setup>
+  
+  <script setup>
 import { computed, onMounted, ref } from "vue";
-import { allTasks } from "../http/task-api";
+import { allTasks, createTask } from "../http/task-api";
+import NewTask from "../components/tasks/NewTask.vue";
 
 const tasks = ref([]);
 
 onMounted(async () => {
-    const { data } = await allTasks();
-    tasks.value = data.data;
+  const { data } = await allTasks();
+  tasks.value = data.data;
 });
 
 const completedTasks = computed(() =>
-    tasks.value.filter((task) => task.is_completed)
+  tasks.value.filter((task) => task.is_completed)
 );
 const uncompletedTasks = computed(() =>
-    tasks.value.filter((task) => !task.is_completed)
+  tasks.value.filter((task) => !task.is_completed)
 );
 
 const showCompletedTask = ref(false);
 
 const showToggleCompleteBtn = computed(
-    () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
+  () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
 );
+
+const handleAddedTask = async (newTask) => {
+  const { data: createdTask } = await createTask(newTask);
+  tasks.value.unshift(createdTask.data);
+};
 </script>
+  
---
+++ src/components/tasks/NewTask.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="task-input">
+    <input
+      type="text"
+      placeholder="+ Add new task. Press enter to save."
+      id="new-task"
+      @keydown.enter="addNewTask"
+    />
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+const newTask = reactive({
+  name: "",
+  is_completed: false,
+});
+
+const emits = defineEmits(["added"]);
+
+const addNewTask = (event) => {
+  if (event.target.value.trim()) {
+    newTask.name = event.target.value;
+    event.target.value = "";
+    emits("added", { ...newTask });
+  }
+};
+</script>
